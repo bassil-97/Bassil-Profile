aea3ed7ce3f1d602719a7911c85f26f6a00a28c8
root <bassilalqadi.ba@gmail.com>
2022-03-02 07:38:18 +0300

    Add span to links

--- src/Components/Navbar/FloatingNav.js
+++ src/Components/Navbar/FloatingNav.js
@@ -7,24 +7,28 @@ export default function FloatingNav() {
   return (
     <div className='floating-nav'>
         <div className='floating-item'>
-            <Link to="/home">
+            <Link to="/home" className='floating-nav-link'>
                 <i className="fa-solid fa-circle-info"></i>
             </Link>
+            <span className='item-title'>Home</span>
         </div>
         <div className='floating-item'>
             <Link to="/resume">
                 <i className="fa-solid fa-file"></i>
             </Link>
+            <span className='item-title'>resume</span>
         </div>
         <div className='floating-item'>
             <Link to="/portfolio">
                 <i className="fa-solid fa-code"></i>
             </Link>
+            <span className='item-title'>portfolio</span>
         </div>
         <div className='floating-item'>
             <Link to="/contact">
                 <i className="fa-solid fa-address-book"></i>
             </Link>
+            <span className='item-title'>contact</span>
         </div>
     </div>
   )

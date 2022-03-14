431c81337b064df10be8c756826992bb5f5074a0
root <bassilalqadi.ba@gmail.com>
2022-03-14 18:28:55 +0300

    Add changes to Floating navbar

--- src/Components/Navbar/FloatingNav.js
+++ src/Components/Navbar/FloatingNav.js
@@ -1,41 +1,36 @@
-import React from 'react';
-import './Navbar.css';
+import React from "react";
+import "./Navbar.css";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function FloatingNav() {
   return (
-    <div className='floating-nav' data-aos="fade-right">
-        <div className='floating-item'>
-            <Link to="/home" className='floating-nav-link'>
-                <i className="fa-solid fa-circle-info"></i>
-            </Link>
-            <span className='item-title'>Home</span>
-        </div>
-        <div className='floating-item'>
-            <Link to="/resume" className='floating-nav-link'>
-                <i className="fa-solid fa-file"></i>
-            </Link>
-            <span className='item-title'>resume</span>
-        </div>
-        <div className='floating-item'>
-            <Link to="/portfolio" className='floating-nav-link'>
-                <i className="fa-solid fa-code"></i>
-            </Link>
-            <span className='item-title'>portfolio</span>
-        </div>
-        <div className='floating-item'>
-            <Link to="/contact" className='floating-nav-link'> 
-                <i className="fa-solid fa-address-book"></i>
-            </Link>
-            <span className='item-title'>contact</span>
-        </div>
-        <div className='floating-item'>
-            <Link to="/pricing" className='floating-nav-link'> 
-                <i className="fa-solid fa-tag"></i>
-            </Link>
-            <span className='item-title'>pricing</span>
-        </div>
+    <div className="floating-nav" data-aos="fade-right">
+      <div className="floating-item">
+        <Link to="/home" className="floating-nav-link">
+          <i className="fa-solid fa-circle-info"></i>
+        </Link>
+      </div>
+      <div className="floating-item">
+        <Link to="/resume" className="floating-nav-link">
+          <i className="fa-solid fa-file"></i>
+        </Link>
+      </div>
+      <div className="floating-item">
+        <Link to="/portfolio" className="floating-nav-link">
+          <i className="fa-solid fa-code"></i>
+        </Link>
+      </div>
+      <div className="floating-item">
+        <Link to="/contact" className="floating-nav-link">
+          <i className="fa-solid fa-address-book"></i>
+        </Link>
+      </div>
+      <div className="floating-item">
+        <Link to="/pricing" className="floating-nav-link">
+          <i className="fa-solid fa-tag"></i>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
